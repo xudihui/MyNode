984a1bf76fb85c7beb8dd6032a79f138796ee7c3
xudihui <xudihui@xudihui.com>
2016-07-11 15:42:11 +0800

    创建http服务器

---
+++ http/http.js
@@ -0,0 +1,18 @@
+/* http请求模块
+   http.js
+
+*/
+
+
+// 引入http模块
+var http = require('http');
+
+http.createServer(function(request,response){
+	request.on('end',function(){ 
+		response.writeHead(200,{'Content-Type':'text/html'});
+		
+	});
+	
+	response.end('Welcome to here!');
+	
+}).listen('8888');
