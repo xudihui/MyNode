5514dd99a905015d1b57b8c3c2d7a416e08ea5b7
xudihui <xudihui@xudihui.com>
2016-07-11 16:41:33 +0800

    modify

--- http/http.js
+++ http/http.js
@@ -10,7 +10,6 @@ var http = require('http');
 http.createServer(function(request,response){
 	request.on('end',function(){ 
 		response.writeHead(200,{'Content-Type':'text/html'});
-		
 	});
 	
 	response.end('Welcome to here!');
